3f40edcb621cdb0e047f9ca022617fb097aefa19
Reena Kumari <reenanishi1994@gmail.com>
2021-07-09 19:30:03 -0700

    todo app inital commit

---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Footer = () => <h1>This is Footer of my page</h1>
+
+
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+class Header extends React.Component
+{
+  render()
+  {
+    return (
+        <div class="jumbotron text-center">
+        <h1> MY TODO LIST</h1>
+      </div>
+    )
+  }
+}
+export default Header;
